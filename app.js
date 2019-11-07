d40833cc6c0d082659c28fd3af6bad565411725f
coughlin.nicholas@gmail.com <you@example.com>
2019-11-07 18:48:36 +0000

    looping over the titles of all star wars movies

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
------------------------------------
-SETUP
------------------------------------
+//-----------------------------------
+//SETUP
+//-----------------------------------
 
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+
+
+//-----------------------------------
+//API REQUESTS
+//-----------------------------------
+
+request('http://swapi.co/api/films/', function (error, response, body) {
+  console.log('error:', error); 
+  console.log('statusCode:', response && response.statusCode); 
+  
+  var parsedBody = JSON.parse(body);
+  
+  console.log(parsedBody["results"][0]["title"]);
+  
+  console.log(parsedBody["results"].length);
+  
+  for(var i=0;i<parsedBody["results"].length;i++){
+  console.log(parsedBody["results"][i]["title"]);
+  }
+  
 });
 
